055ac73c1fbb82512564396b95d8fff7fa0f8f4c
Angela Gathu <agathu87@gmail.com>
2017-09-30 17:10:55 +0300

    edit main files

--- app/routes/questions.js
+++ app/routes/questions.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return [{
+      question: 'George Bush or Bill Clinton?',
+      answer: 'Bill Clinton',
+      author: 'B.C',
+      notes: 'Tests how conservative or liberal you are.',
+    }];
+  }
 });
