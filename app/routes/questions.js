57337a08fa3bcef1bdc362a65cb7f679b65174f8
Angela Gathu <agathu87@gmail.com>
2017-10-03 11:00:43 +0300

    make other changes

--- app/routes/questions.js
+++ app/routes/questions.js
@@ -5,28 +5,28 @@ export default Ember.Route.extend({
     return [{
         title: '',
         question: 'George Bush or Bill Clinton?',
-        answer: 'Bill Clinton',
+
         author: 'A.G',
         notes: 'Tests how conservative or liberal you are.'
       },
 
       {
         question: 'Perception or Reality?',
-        answer: 'Perception',
+
         author: 'A.G',
         notes: 'Tests how you view life.'
       },
 
       {
         question: 'Observant or Intuitive?',
-        answer: 'Intuitive',
+
         author: 'A.G',
         notes: 'Tests how you become aware of your environment.'
       },
 
       {
         question: 'Extrovert or Introvert?',
-        answer: 'Extrovert',
+
         author: 'A.G',
         notes: 'Tests whether you gain energy from people or self.'
       }
