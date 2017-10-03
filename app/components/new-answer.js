987599668963972426f5517e2b17cb2ec32080fd
Angela Gathu <agathu87@gmail.com>
2017-10-03 17:20:45 +0300

    make other changes

--- app/components/new-answer.js
+++ app/components/new-answer.js
@@ -1,4 +1,28 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  showNewAnswer: false,
+  actions: {
+    newAnswer() {
+      this.set('showNewAnswer', true)
+    },
+    hideNewAnswer() {
+      this.set('showNewAnswer', false),
+        this.set('actualAnswer', ''),
+        this.set('author', '')
+    },
+    saveNewAnswer: function() {
+      var params = {
+        actualAnswer: this.get('actualAnswer'),
+        author: this.get('author'),
+        voteCount: 0,
+        question: this.get('question')
+      };
+      //console.log(params);
+      this.sendAction('saveNewAnswer', params),
+        this.set('actualAnswer', ''),
+        this.set('author', ''),
+        this.set('showNewAnswer', false)
+    }
+  }
 });
