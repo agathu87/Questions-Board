b034a57842abc55296d4b88cf2f21c57e878953c
Angela Gathu <agathu87@gmail.com>
2017-10-03 11:00:27 +0300

    update file

--- app/routes/answers.js
+++ app/routes/answers.js
@@ -1,4 +1,36 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return [{
+        title: '',
+
+        answer: 'Bill Clinton',
+        author: 'A.G'
+
+      },
+
+      {
+
+        answer: 'Perception',
+        author: 'A.G'
+
+      },
+
+      {
+
+        answer: 'Intuitive',
+        author: 'A.G'
+
+      },
+
+      {
+
+        answer: 'Extrovert',
+        author: 'A.G'
+
+      }
+    ];
+  }
+
 });
