e4bd9d81c97ebb4ac179f16a49fcd2fb3fe82ef5
Angela Gathu <agathu87@gmail.com>
2017-10-05 12:19:07 +0300

    add function

---
+++ app/components/question-detail.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  actions: {
+    updateQuestion(question, params) {
+      alert('reched');
+      this.sendAction('updateQuestion', question, params)
+    }
+  }
+});
